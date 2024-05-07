2e32c17c84cbe60ba46a419fc68b8936130c4c60
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2024-05-07 18:17:36 +0800

    Add project files.

---
+++ README.md
@@ -0,0 +1,6 @@
+## How to debug
+
+1. Run `copyLib.ps1`.
+2. Build the project.
+3. Run `debug.ps1`.
+4. Attach to the process `PowerToys.PowerLauncher`.
---
+++ TestSettings/Main.cs
@@ -0,0 +1,117 @@
+using ManagedCommon;
+using Microsoft.PowerToys.Settings.UI.Library;
+using System.Windows.Controls;
+using Wox.Plugin;
+
+namespace Community.PowerToys.Run.Plugin.TestSettings
+{
+    public class Main : IPlugin, IPluginI18n, ISettingProvider, IReloadable, IDisposable
+    {
+        private const string Setting = nameof(Setting);
+
+        // current value of the setting
+        private bool _setting;
+
+        private PluginInitContext _context;
+
+        private string _iconPath;
+
+        private bool _disposed;
+
+        public string Name => "Test";
+
+        public string Description => "Test";
+
+        public static string PluginID => "7a13c669be7d4ec7a935f1ff19d2319f";
+
+        public IEnumerable<PluginAdditionalOption> AdditionalOptions => new List<PluginAdditionalOption>()
+        {
+            new ()
+            {
+                PluginOptionType = PluginAdditionalOption.AdditionalOptionType.Numberbox,
+                Key = Setting,
+                DisplayLabel = "Test NumberBox",
+                NumberValue = 5,
+                NumberBoxMin = 2,
+            },
+        };
+
+        public void UpdateSettings(PowerLauncherPluginSettings settings)
+        {
+            _setting = settings?.AdditionalOptions?.FirstOrDefault(x => x.Key == Setting)?.Value ?? false;
+        }
+
+        public List<Result> Query(Query query)
+        {
+            return new List<Result>();
+        }
+
+        public void Init(PluginInitContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _context.API.ThemeChanged += OnThemeChanged;
+            UpdateIconPath(_context.API.GetCurrentTheme());
+        }
+
+        public string GetTranslatedPluginTitle()
+        {
+            return "Test";
+        }
+
+        public string GetTranslatedPluginDescription()
+        {
+            return "Test";
+        }
+
+        private void OnThemeChanged(Theme oldtheme, Theme newTheme)
+        {
+            UpdateIconPath(newTheme);
+        }
+
+        private void UpdateIconPath(Theme theme)
+        {
+            if (theme == Theme.Light || theme == Theme.HighContrastWhite)
+            {
+                _iconPath = "Images/TestSettings.light.png";
+            }
+            else
+            {
+                _iconPath = "Images/TestSettings.dark.png";
+            }
+        }
+
+        public Control CreateSettingPanel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReloadData()
+        {
+            if (_context is null)
+            {
+                return;
+            }
+
+            UpdateIconPath(_context.API.GetCurrentTheme());
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed && disposing)
+            {
+                if (_context != null && _context.API != null)
+                {
+                    _context.API.ThemeChanged -= OnThemeChanged;
+                }
+
+                _disposed = true;
+            }
+        }
+    }
+}
